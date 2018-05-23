59a7da52434e2a04398b8668725387a28f736625
unknown <roman.mokrinskij@gmail.com>
2018-05-23 10:13:57 +0300

    fix registration form

--- views/user/registration.php
+++ views/user/registration.php
@@ -4,29 +4,28 @@ use yii\widgets\ActiveForm;
 use yii\helpers\Html;
 ?>
 <div class="row">
-    <div class="col-xs-12">
+    <div class="col-xs-4 col-xs-offset-4">
         <div class="registration__title-wrapper">
             <h3>Регистрация</h3>
         </div>
+        <?php $form = ActiveForm::begin(); ?>
+        <?php $form->fieldConfig = ['inputOptions' => ['autocomplete' => 'off', 'class' => 'form-control']]; ?>
+            <?= $form->field($user, 'email')->label('Email'); ?>
+            <?= $form->field($user, 'password')->label('Пароль'); ?>
+            <?= $form->field($user, 'confirm_password')->label('Подтверждение пароля'); ?>
+            <?= $form->field($user, 'name')->label('Имя'); ?>
+            <?= $form->field($user, 'surname')->label('Фамилия'); ?>
+            <?= $form->field($user, 'phone')->label('Телефон'); ?>
+            <?= $form->field($user, 'work_phone')->label('Рабочий телефон'); ?>
+            <?= $form->field($user, 'city')->label('Город'); ?>
+            <?= $form->field($user, 'street')->label('Улица'); ?>
+            <?= $form->field($user, 'house_number')->label('Дом'); ?>
+            <?= $form->field($user, 'office_number')->label('Офис\\Квартира'); ?>
+            <?= $form->field($user, 'company')->label('Компания'); ?>     
+            <div class="form-group">
+                <?= Html::submitButton('Регистрация', ['class' => 'btn btn-primary']);?>
+            </div>
+        <?php ActiveForm::end(); ?>
     </div>
-    <?php $form = ActiveForm::begin(); ?>
-        <?= $form->field($user, 'email', ['template' => '{input}{error}'])->textInput(['class' => 'Email']); ?>
-        <?= $form->field($user, 'password'); ?>
-        <?= $form->field($user, 'confirm_password'); ?>
-        <?= $form->field($user, 'name'); ?>
-        <?= $form->field($user, 'surname'); ?>
-        <?= $form->field($user, 'patronymic'); ?>
-        <?= $form->field($user, 'phone'); ?>
-        <?= $form->field($user, 'work_phone'); ?>
-        <?= $form->field($user, 'city'); ?>
-        <?= $form->field($user, 'street'); ?>
-        <?= $form->field($user, 'house_number'); ?>
-        <?= $form->field($user, 'office_number'); ?>
-        <?= $form->field($user, 'company'); ?>     
-        <div class="form-group">
-            <?= Html::submitButton('Регистрация', ['class' => 'btn btn-primary']);?>
-        </div>
-    <?php ActiveForm::end(); ?>
-    
 </div>
 <?php $this->registerJsFile('@web/js/user/registration.js'); ?>
